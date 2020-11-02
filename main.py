2d4c4c57607a6fcc26745553e52490d2958fde83
nateUHHS <54079458+nateUHHS@users.noreply.github.com>
2020-11-02 16:08:17 -0500

    Update main.blocks, main.ts, main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 while True:
-    print(input.light_level())
-    
-    if input.light_level() > 555:
+    # print(input.light_level())
+    # if input.light_level() > 55:
+    # light.set_all(0xff0080)
+    # else:
+    # light.set_all(0xff8000)
+    print(input.sound_level())
+    if input.sound_level() > 125:
         light.set_all(0xff0080)
     else:
         light.set_all(0xff8000)
--- main.ts
+++ main.ts
@@ -1,6 +1,11 @@
 while (true) {
-    console.log(input.lightLevel())
-    if (input.lightLevel() > 555) {
+    //  print(input.light_level())
+    //  if input.light_level() > 55:
+    //  light.set_all(0xff0080)
+    //  else:
+    //  light.set_all(0xff8000)
+    console.log(input.soundLevel())
+    if (input.soundLevel() > 125) {
         light.setAll(0xff0080)
     } else {
         light.setAll(0xff8000)
